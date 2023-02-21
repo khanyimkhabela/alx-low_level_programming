b4919031a386ec274cab94c3d673791fecbb6b60
khanyimkhabela <khanyimkhabela980@gmail.com>
2023-02-21 14:09:59 -0800

    prints putchar followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
-#define "main_h"
+
 /**
- * main - prints _putchar, followed by a new line
- * Return: Always 0
+* main - 'prints _putchar, followed by a new line'
+* Return: Always 0
  */
+
 int main(void)
 {
-_putcahar('_');
-_putcahar('p');
-_putcahar('u');
-_putcahar('t');
-_putcahar('c');
-_putcahar('h');
-_putcahar('a');
-_putcahar('r');
-_putcahar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
